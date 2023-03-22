887612ada788a5965756a9974cdd7a05004eecee
Daniel Guerra <danielguerra@iMac-de-Daniel.local>
2023-03-22 13:35:10 -0300

    Frontend dependencys installed

--- fulltime-test-front/src/App.js
+++ fulltime-test-front/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import MainContent from './components/MainContent'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <MainContent/>
       </header>
     </div>
   );
---
+++ fulltime-test-front/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import {Typography} from '@mui/material'
+
+export default function MainContent(props){
+    return(
+        <div className="container">
+            <div className="rounded bg-light py-5 border shadow-sm">
+                <Typography variant="body1" style={{fontFamily:"roboto"}}>Technical Test Fulltime-Force</Typography>
+            </div>
+        </div>
+    )
+}
